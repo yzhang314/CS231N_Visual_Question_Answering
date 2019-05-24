d2fd7878b7a5b4796faae4835f7f1450f2c668db
zewenzhang <zwzhang18@gmail.com>
2019-05-23 23:53:12 -0700

    name the output text

--- train.py
+++ train.py
@@ -28,6 +28,7 @@ def train(model, train_loader, eval_loader, num_epochs, output):
     logger = utils.Logger(os.path.join(output, 'log.txt'))
     best_eval_score = 0
     f = open("result5.txt", "w+")
+    f.write('name of the project:', ''))
 
     for epoch in range(num_epochs):
         total_loss = 0
